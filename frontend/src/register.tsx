f5bb3d5be7daa3100463632056d7c6613020f42b
Ashwin Pathi <AshwinPathi@users.noreply.github.com>
2020-10-12 22:53:40 -0400

    jsencrypt and register component

---
+++ frontend/src/register.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Register() {
+  return (
+    <div className="Register">
+    	
+	</div>
+  );
+}
+
+export default Register;
