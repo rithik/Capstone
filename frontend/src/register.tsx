368040f9f3c5351cd970dabaff7bc37973318f0b
Ashwin Pathi <AshwinPathi@users.noreply.github.com>
2020-10-15 11:23:26 -0400

    Created register button on index page

--- frontend/src/register.tsx
+++ frontend/src/register.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import generateKeys from './utils/generateKeys'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField'
+
 
 function Register() {
-  return (
-    <div className="Register">
-    	
-	</div>
-  );
+	const [myUsernameValue, setUsernameValue] = useState('') 
+	return (
+		<div className="Register">
+			<TextField 
+				InputProps={{style: {color: "white"} }} 
+				id="outlined-basic" label="Username" 
+				variant="outlined" 
+				value={myUsernameValue}
+				onChange={(e) => setUsernameValue(e.target.value)} 
+			/>
+			<Button onClick={(e) => generateKeys(myUsernameValue, e)} variant="contained" color="primary">
+			  Register
+			</Button>
+		</div>
+	);
 }
 
 export default Register;
