96fda612e11b2b21207fd880a545aabac25c7d56
Rithik Yelisetty <rithik@gmail.com>
2020-10-30 18:18:53 -0400

    Encryption works

--- frontend/src/components/ChatFeed.react.js
+++ frontend/src/components/ChatFeed.react.js
@@ -6,7 +6,8 @@ import ClipLoader from "react-spinners/ClipLoader";
 import { css } from "@emotion/core";
 import { gql, useMutation } from '@apollo/client';
 import JSEncrypt from 'jsencrypt';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import CryptoJS from "crypto-js";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const override = css`
@@ -24,46 +25,18 @@ const SEND_MESSAGE = gql`
     }
 `;
 
-
-// https://stackoverflow.com/questions/57068850/how-to-split-a-string-into-chunks-of-a-particular-byte-size
-function chunk(s, maxBytes) {
-    let buf = Buffer.from(s);
-    const result = [];
-    while (buf.length) {
-        let i = buf.lastIndexOf(32, maxBytes+1);
-        // If no space found, try forward search
-        if (i < 0) i = buf.indexOf(32, maxBytes);
-        // If there's no space at all, take the whole string
-        if (i < 0) i = buf.length;
-        // This is a safe cut-off point; never half-way a multi-byte
-        result.push(buf.slice(0, i).toString());
-        buf = buf.slice(i+1); // Skip space (if any)
-    }
-    return result;
-}
-
 function encryptMessage(message, type){
-    var message_to_encrypt_json = {"message": message, "type":type}
-    var message_to_encrypt_string = JSON.stringify(message_to_encrypt_json);
-    var chunks = chunk(message_to_encrypt_string, 500);
-    console.log(chunks);
-    var crypt = new JSEncrypt();
-    var publicKey = "-----BEGIN PUBLIC KEY-----MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAweyAiCo+w3Gxbdj72kPWYm36buUIbkTLHhnhni7nGcp53Rui8paKqr6VIRtJBKqwSOvNz2EqBkikGA5Knj4kQWlihqOJ3clTvk/Di+TnBAiNxUikxb4lOrLR15LZ1+1i1RCZXd1dTCqqvuddiPLMLbo8FG+ppvxBrlWQXNOY+8/7sQJuJpHdneweZuyD9KVeUJgHzBl3gseMaKGB21CUzeXYQYR49wbZem7+fKJEPnQqWZrdxui13YLTJ+FzFDUusSYTUgzBDHBUZwlBb4uo5cct5gyhFqO9Lk5RyNL3ei/+gIdQCDWW7TL91L1FM8ICqnWtTfHKwN1qz/1zUa6scEkJO8SPUY3qQaPJy3pYWYxD87ItW22RL+zCEA3Qy/XwgTHxBV+rET5V8cTDzLlF9WiDSYbW60YtxahDiedMTkmB3CwJVc8oidGLtAxIGWBIzes9HTRjLtNlVBM8D5uSRbz515LLRnEp98RSt7S2L0EjNhD4NsWYL7vHNQPeYaIiDUdMKox8hX9q360S+uE3shksETwwrshZdcafagRUZmTfipuetlTWrOlolbGwajA2iUq4VeMEzJNOI0A8Oe2oLEZZNtNVDvXrL8LNsLXj5cIdRoEMOm0JD892SrdU/Jx6tJQNrZ+agpkn9wppdKvqAqrmIBEl3bnLCmwXOAA8PU0CAwEAAQ==-----END PUBLIC KEY-----";
-    crypt.setPublicKey(publicKey);
-    var enc_text = "";
-    for(var i = 0; i < chunks.length; i++){
-        enc_text += crypt.encrypt(chunks[i]);
-    }
-    return enc_text
+    var messageJSON = {"message": message, "type":type}
+    var messageString = JSON.stringify(messageJSON);
+    const groupPrivateKey = localStorage.getItem('user-privateKey');
+    const encrypted = CryptoJS.AES.encrypt(messageString, groupPrivateKey).toString();
+    return encrypted;
 }
 
 function decryptMessage(message){
-    var crypt2 = new JSEncrypt();
-    var privateKey = localStorage.get('user-privateKey');
-    crypt2.setPrivateKey(privateKey);
-    var dec_message = crypt2.decrypt(message);
-    var dec_message_json = JSON.parse(dec_message);
-    return [dec_message_json["message"], dec_message_json["type"]]
+    const groupPrivateKey = localStorage.getItem('user-privateKey');
+    const decrypt = CryptoJS.AES.decrypt(message, groupPrivateKey);
+    return decrypt.toString(CryptoJS.enc.Utf8);
 }
 
 
@@ -84,7 +57,7 @@ function ChatFeed({
     const reversedEntries = [].concat(entries.messagesByGroup).reverse();
     const username = localStorage.getItem('username');
     const messages = reversedEntries.map(message => {
-        return new Message({ id: message.sender === username ? 0 : message.sender, message: message.content, senderName: `@${message.sender}` })
+        return new Message({ id: message.sender === username ? 0 : message.sender, message: decryptMessage(message.content), senderName: `@${message.sender}` })
     })
     useEffect(() => {
         if (firstLoad) {
@@ -102,7 +75,6 @@ function ChatFeed({
             }
         }, 1000);
     }, [firstLoad, setFirstLoad, onLoadMore, messagesStartRef, messagesEndRef]);
-    console.log(entries);
     return <div style={{ marginLeft: '10px', marginRight: '10px', marginBottom: '50px' }}>
         <div style={{ height: '30px' }}
             ref={(el) => { messagesStartRef = el; }}>
