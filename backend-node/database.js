c23327c5427248fa4a9a49f57f3389f7636c9b03
Rithik Yelisetty <rithik@gmail.com>
2020-11-02 17:35:36 -0500

    SSL

--- backend-node/database.js
+++ backend-node/database.js
@@ -6,8 +6,10 @@ const db = production ? new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
-        ssl: true, 
-        rejectUnauthorized: false
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
 }) : new Sequelize('capstone', null, null, {
     dialect: 'sqlite',
