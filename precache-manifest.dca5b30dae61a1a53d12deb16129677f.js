979cae81ca2c4bebd72e81df18d63227a23da13d
rithik <rithik@users.noreply.github.com>
2020-12-07 21:36:02 +0000

    deploy: 7659c48b8aec860769ff1cd2219266d956508411

---
+++ precache-manifest.dca5b30dae61a1a53d12deb16129677f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62832310a9b3fcd06945906a0bf06fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5da39a22e95d4188e796",
+    "url": "./static/css/2.cfcd614e.chunk.css"
+  },
+  {
+    "revision": "553f7f67c2e5adc8ebcd",
+    "url": "./static/css/main.5f149421.chunk.css"
+  },
+  {
+    "revision": "5da39a22e95d4188e796",
+    "url": "./static/js/2.09467249.chunk.js"
+  },
+  {
+    "revision": "0e9a4c6d265e50b8c57e2c634583c241",
+    "url": "./static/js/2.09467249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "553f7f67c2e5adc8ebcd",
+    "url": "./static/js/main.f384bd50.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  }
+]);
